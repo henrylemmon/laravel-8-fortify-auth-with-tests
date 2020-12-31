c28bae6f07b96910559970f51ec1919eb44ac406
henrylemmon <henrylemmon@yahoo.com>
2020-12-30 22:16:11 -0800

    fix file input label text

--- resources/views/components/forms/file-input.blade.php
+++ resources/views/components/forms/file-input.blade.php
@@ -3,7 +3,7 @@
 ])
 
 <div x-data="{ validate: true }" class="file-input">
-    <label class="file-label" for="file">Select Image<p class="file-name"></p></label>
+    <label class="file-label" for="file">Select&nbsp;Image<p class="file-name"></p></label>
     <input @change="validate = false" class="file" type="file" id="file" name="image">
     @error('image')
         <span x-show="validate" class="error-text">{{ $message }}</span>
