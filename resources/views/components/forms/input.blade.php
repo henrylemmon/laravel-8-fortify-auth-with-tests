d730d0340046c90bf72c6ec64c4f8e31e087ac50
henrylemmon <henrylemmon@yahoo.com>
2020-12-31 23:23:48 -0800

    fix the value of the reset imput

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -8,7 +8,7 @@
 
 <div class="form-element-group">
     <label class="form-label" for="{{ $id }}">{{ $label }}</label>
-    <input value="{{ old($name) }}" class="form-input @error($name) error-input @enderror" type="{{ $type }}" id="{{ $id }}" name="{{ $name }}" @if(! empty($value)) value="{{ $value }} @endif">
+    <input value="{{ old($name) }}" class="form-input @error($name) error-input @enderror" type="{{ $type }}" id="{{ $id }}" name="{{ $name }}" @if(! empty($value)) value="{{ $value }}" @endif>
     @error($id)
         <span class="error-text">{{ $message }}</span>
     @enderror
